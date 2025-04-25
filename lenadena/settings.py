cc753d10bc8e1d2ecec697cda42a7f1c1636099c
Chit1998 <cool.chitransh83@gmail.com>
2025-04-25 11:46:40 +0530

    .env file changes

--- lenadena/settings.py
+++ lenadena/settings.py
@@ -22,7 +22,6 @@ load_dotenv(BASE_DIR /'.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-u-tgv+n+w3wrtx%6om&%p!h^sl@!qu11mx=1%5^hy@_&dw%#po'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -92,20 +91,6 @@ WSGI_APPLICATION = 'lenadena.wsgi.application'
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hisab_kitab',
-#         'USER': 'root',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }
 #     }
 # }
 
